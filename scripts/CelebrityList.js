4de1164501034132ec40aa71fc18e6cf15214be1
Ethan Brown <eb934053@gmail.com>
2024-12-09 12:05:02 -0600

    Completed Self-Assessment

--- scripts/CelebrityList.js
+++ scripts/CelebrityList.js
@@ -7,14 +7,29 @@ export const Celebrities = () => {
 
     for (const star of celebrities) {
         html += `<li 
-                    data-id="${celebrity.id}" 
+                    data-id="${star.id}" 
                     data-type="celebrity"
-                    data-sport="${celebrty.sport}"
-                    id="star--${celebrity.id}">
-                    ${celebrity.name}
+                    data-sport="${star.sport}">
+                    ${star.name}
                 </li>`
     }
 
     html += "</ol>"
     return html
 }
+
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const starClicked = clickEvent.target
+
+        if(starClicked.dataset.type === "celebrity") {
+            for (const celebrity of celebrities) {
+                if(celebrity.id === parseInt(starClicked.dataset.id)) {
+                    window.alert(`${celebrity.name} is a ${celebrity.sport} star.`)
+                }
+            }
+        }
+    }
+)
--- scripts/Kids.js
+++ scripts/Kids.js
@@ -6,10 +6,24 @@ export const Kids = () => {
     let html = "<ol>"
 
     for (const child of children) {
-        html += `<li data-id="${child.id}" data-type="child" data-wish="${child.wish}">${child,name}</li>`
+        html += `<li data-id="${child.id}" data-type="child" data-wish="${child.wish}">${child.name}</li>`
     }
 
     html += "</ol>"
     return html
 }
 
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const kidClicked = clickEvent.target
+
+        if(kidClicked.dataset.type === "child") {
+            for (const child of children) {
+                if(child.id === parseInt(kidClicked.dataset.id)) {
+                    window.alert(`${child.name}'s wish is to ${child.wish}`)
+                }
+            }
+        }
+    }
+)
